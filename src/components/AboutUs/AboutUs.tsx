73e2c965f0887c831b6a8900dd923d52530ba7d8
Tatiana Duran <tatiana.duran@N-000-105>
2023-11-10 20:45:56 -0300

    corrección imagenes

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -8,13 +8,13 @@ const AboutUs = ()=>{
             <p>We are a team of passionate individuals who love to create amazing things. Our mission is to provide high-quality services and products that exceed our customers' expectations.</p>
             <div className="row mt-2">
                 <div className="col-md-4 col-12">
-                    <Cards title="Card 1" image="src/assets/images/card 1.jpg"></Cards>
+                    <Cards title="Card 1" image="https://i.pinimg.com/564x/d9/4f/79/d94f79d53e79b686b325ca6d437ca6aa.jpg"></Cards>
                 </div>
                 <div className="col-md-4 col-12">
-                    <Cards title="Card 2" image="src/assets/images/card 2.jpg"></Cards>
+                    <Cards title="Card 2" image="https://i.pinimg.com/564x/3a/68/c7/3a68c708ab0660e7b3d8a5b92874bf63.jpg"></Cards>
                 </div>
                 <div className="col-md-4 col-12">
-                    <Cards title="Card 3" image="src/assets/images/card 3.jpg"></Cards>
+                    <Cards title="Card 3" image="https://i.pinimg.com/564x/2d/da/48/2dda48be0dbd2cb168e95974a0d70e7e.jpg"></Cards>
                 </div>
             </div>
         </div>
--- src/components/CarouselHome/CarouselHome.tsx
+++ src/components/CarouselHome/CarouselHome.tsx
@@ -7,7 +7,7 @@ const CarouselHome=()=>{
                 <img 
                     className='d-block w-100' 
                     style={{maxHeight:"500px", objectFit:'cover'}} 
-                    src="src/assets/images/slide1.jpg" 
+                    src="https://okdiario.com/img/2017/06/19/maravillas-naturales-del-mundo-8.jpg" 
                     alt="First Slide" />
                 <Carousel.Caption>
                 <h3>First slide label</h3>
@@ -18,7 +18,7 @@ const CarouselHome=()=>{
                 <img 
                     className='d-block w-100' 
                     style={{maxHeight:"500px", objectFit:'cover'}} 
-                    src="src/assets/images/slide2.jpg" 
+                    src="https://st1.uvnimg.com/c8/a4/79af1ab2405eae3bf2cd1fb85727/istock-930824730.jpg" 
                     alt="Second Slide" />
                 <Carousel.Caption>
                 <h3>Second slide label</h3>
@@ -29,7 +29,7 @@ const CarouselHome=()=>{
                 <img 
                     className='d-block w-100' 
                     style={{maxHeight:"500px", objectFit:'cover'}} 
-                    src="src/assets/images/slide3.jpg" 
+                    src="https://www.viajandoabrasil.com/wp-content/uploads/2020/12/foz-iguazu.jpg" 
                     alt="Third Slide" />
                 <Carousel.Caption>
                 <h3>Third slide label</h3>
@@ -40,7 +40,7 @@ const CarouselHome=()=>{
                 <img 
                     className='d-block w-100' 
                     style={{maxHeight:"500px", objectFit:'cover'}} 
-                    src="src/assets/images/slide4.jpg" 
+                    src="https://i.pinimg.com/originals/1e/59/38/1e59388bc320dc56e159d00e20743f00.jpg" 
                     alt="Fourth Slide" />
                 <Carousel.Caption>
                 <h3>Fourth slide label</h3>
